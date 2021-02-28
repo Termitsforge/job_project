c4f2e06265389534f37cf267fd673e4255db3bfb
Termitsforge <medic573@gmail.com>
2021-02-28 15:26:09 +0300

    v0.2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let text = document.querySelector("#text"),
     buttons = document.querySelectorAll("button"),
     arrayAnimals = [];
 
+/* Класс Животное */
 class Animal {
     constructor(name, question, answer) {
         this.name = name;
@@ -17,15 +18,16 @@ class Animal {
         console.log(`Name : ${this.name}; Question : ${this.question};  Answer : ${this.answer};`);
     }
 }
-
+/*Функция создание объекта и добавление его в массив*/
 const objectFactory = (name, question, answer) => {
     arrayAnimals.push(new Animal(name, question, answer));
 }
 
-objectFactory("Барсик", "Это кот ?", true);
+objectFactory("Кот", "У него много шерсти ?", true);
 objectFactory("Жираф", "У него большая шея ?", true);
 objectFactory("Волк", "У него большие зубы ?", true);
 
+/* Стартовая страница */
 const start = () => {
     text.textContent = "Привет, загадай животное";
     buttons[0].textContent = "Старт";
@@ -40,25 +42,99 @@ const start = () => {
         }
     };
 };
+/*Добавление нового животного*/
+const AddNewAnimal = () => {
+    let mainBox = document.querySelector(".main__box"),
+        addAnimalBox = document.querySelector(".addNewAnimal"),
+        inputsText = document.querySelectorAll("input"),
+        radioButtuns = document.getElementsByName("answer"),
+        button = document.querySelector(".AddAnimal");
+    mainBox.style.display = "none";
+    addAnimalBox.style.display = "block";
+    button.onclick = function () {
+        let answer;
+        if (radioButtuns[0].checked) {
+            answer = true;
+            radioButtuns[0].checked = false;
+        } else {
+            answer = false;
+            radioButtuns[1].checked = false;
+        }
 
+        objectFactory(inputsText[0].value, inputsText[1].value, answer);
+        inputsText[0].value = "";
+        inputsText[1].value = "";
+        mainBox.style.display = "block";
+        addAnimalBox.style.display = "none";
+        start();
+        for (let i = 0; i < arrayAnimals.length; i++) {
+            arrayAnimals[i].toString();
+        }
+    }
+
+}
+/*Станица с перечислением вопросов*/
 let i = 0;
 const question = () => {
-    text.textContent = arrayAnimals[i].question;
-    buttons[0].textContent = "Да";
-    buttons[1].textContent = "Нет";
+    /*Проверка массива */
+    if (arrayAnimals.length === 0) {
+        text.textContent = "Я не знаю ни одного животного. Расскажи)";
+        buttons[0].textContent = "Слушай";
+        buttons[1].textContent = "Пока";
+        buttons[0].onclick = function () {
+            i = 0;
+            AddNewAnimal();
 
-    buttons[0].onclick = function () {
-        console.log("ПОБЕДА");
-    };
-    buttons[1].onclick = function () {
-        if (i === arrayAnimals.length - 1) {
-            console.log("Я сдаюсь");
-        } else {
-            i++;
-            question();
-        }
-        
-    };
+        };
+        buttons[1].onclick = function () {
+            i = 0;
+            start();
+        };
+
+    } else if (arrayAnimals.length === 1) {
+        text.textContent = `Я знаю только ${arrayAnimals[0].name}. Ты его загадал ?`;
+        buttons[0].textContent = "Верно";
+        buttons[1].textContent = "Не верно";
+
+        buttons[0].onclick = function () {
+            i = 0;
+            start();
+        };
+        buttons[1].onclick = function () {
+            i = 0;
+            AddNewAnimal();
+        };
+
+    } else {
+        text.textContent = arrayAnimals[i].question;
+        buttons[0].textContent = "Да";
+        buttons[1].textContent = "Нет";
+
+        buttons[0].onclick = function () {
+            text.textContent = `Это ${arrayAnimals[i].name} ?`;
+            buttons[0].textContent = "Верно";
+            buttons[1].textContent = "Не верно";
+
+            buttons[0].onclick = function () {
+                i = 0;
+                start();
+            };
+            buttons[1].onclick = function () {
+                i = 0;
+                AddNewAnimal();
+            };
+
+        };
+        buttons[1].onclick = function () {
+            if (i === arrayAnimals.length - 1) {
+                console.log("Я сдаюсь");
+            } else {
+                i++;
+                question();
+            }
+        };
+    }
+
 }
 
 start();
